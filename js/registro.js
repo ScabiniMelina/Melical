251ff05969f166936fd3f4860a2004db59ed16d7
Scabini Melina <60904748+ScabiniMelina@users.noreply.github.com>
2021-06-14 11:11:10 -0300

    Funciona el boton de mostrar y ocultar contraseña

--- js/registro.js
+++ js/registro.js
@@ -1,33 +1,35 @@
+let  formularioRegistro = document.getElementById('formularioRegistro');
+let  botonRegistro = document.getElementById('botonRegistro');
+let  botonMostrarContrasena =document.getElementById('show_password')
+let inputContrasena = document.getElementById("txtPassword");
 
-  let  formularioRegistro = document.getElementById('formularioRegistro');
-  let  botonRegistro = document.getElementById('botonRegistro');
-  botonRegistro.addEventListener('click',()=>{
-    const datosForm = new FormData(formularioRegistro);
-    console.log(datosForm);
-      fetch('../php/registro.php',{
-          method: 'POST',
-          body: datosForm, 
-      })
-      .then(respuesta => respuesta.text())
-      .then(data => alert(data));
-  
+document.addEventListener("DOMContentLoaded",()=>{
+  botonMostrarContrasena.addEventListener("click",()=>{
+    mostrarPassword()
   })
+})
 
-// function mostrarPassword(){
-//     let cambio = document.getElementById("txtPassword");
-//     if(cambio.type == "password"){
-//       cambio.type = "text";
-//       document.querySelector('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-//     }else{
-//       cambio.type = "password";
-//       document.querySelector('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
-//     }
-//   } 
+botonRegistro.addEventListener('click',()=>{
+  const datosForm = new FormData(formularioRegistro);
+  console.log(datosForm);
+    fetch('../php/registro.php',{
+        method: 'POST',
+        body: datosForm, 
+    })
+    .then(respuesta => respuesta.text())
+    .then(data => alert(data));
+
+})
+
+function mostrarPassword(){
+  if(inputContrasena.type == "password"){
+    inputContrasena.type = "text";
+    document.querySelector('.icon').classList.replace('fa-eye-slash','fa-eye');
+  }else{
+    inputContrasena.type = "password";
+    document.querySelector('.icon').classList.replace('fa-eye','fa-eye-slash');
+  } 
+} 
   
-//   // $(document).ready(function () {
-//   // //CheckBox mostrar contraseña
-//   // $('#ShowPassword').click(function () {
-//   //   $('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
-//   // });
-// });
+
 
--- php/Registro.php
+++ php/Registro.php
@@ -1,17 +1,17 @@
 <?php
 include("conexion.php");
-$dni = $POST['dni']; 
-$nombre = $POST['nombre'];
-$apellido = $POST['apellido'];
-$fechaNacimieto = $POST['fechaNacimiento'];
-$direccion = $POST['direccion'];
-$direccionN = $POST['direccionN'];
-$telefono = $POST['telefono'];
-$email = $POST['email'];
-$contrasena = $POST['contrasena'];
-$sql = "INSERT INTO PERSONAL_INFORMATION VALUES ($dni,$nombre,$apellido,$fechaNacimieto,$email) ";
+$dni = $_POST['dni']; 
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$fechaNacimieto = $_POST['fechaNacimiento'];
+$direccion = $_POST['direccion'];
+$direccionN = $_POST['direccionN'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+$contrasena = $_POST['contrasena'];
+$sql = "INSERT INTO PERSONAL_INFORMATION (ID_DNI,name,surname,date_birth,phone,email) VALUES ($dni,$nombre,$apellido,$fechaNacimieto,$telefono,$email)";
 $consultaSql = $conexion->query($sql);
-$resultado = $consultaSql->fetch_all(MYSQLI_ASSOC);
+// $resultado = $consultaSql->fetch_all(MYSQLI_ASSOC);
 
 // $sql = "INSERT INTO USER VALUES ($dni,$nombre,$apellido,$fechaNacimieto,$email) ";
 // $consultaSql = $conexion->query($sql);
