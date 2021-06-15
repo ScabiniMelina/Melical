81bffb7cc4e83324376d6fa76dbec01cc829b51c
Scabini Melina <60904748+ScabiniMelina@users.noreply.github.com>
2021-06-15 15:02:56 -0300

    se traen los datos del paciente que estan en la bd a la interfaz de info general

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   activarFuncionesMenu();
   // document.addEventListener("click",(e)=>{
   //   console.log(e.target)
+  // let iconos = document.querySelectorAll("#contenedorFiltros i")
+  // console.log(iconos)
   // })
   //sidebar
   document.getElementById("cronograma").addEventListener("click",()=>{
@@ -91,12 +93,14 @@ async function cargarBotonesBusquedaPacientes(){
   document.getElementById("tablaPacientes").addEventListener("click",(e)=>{
     data = new FormData();
     data.append('ID_DNI', e.target.parentNode.dataset.id);
-    fetch("../php/obtenerAntecedentesInfoGeneralHistoriaClinica.php",{
+    console.log(e.target.parentNode)
+    console.log(e.target.parentNode.dataset.id)
+    mostrarSeccion("paciente.html","Paciente")
+    .then(fetch("../php/obtenerAntecedentesInfoGeneralHistoriaClinica.php",{
         method:"POST", 
         body:data})
         .then(respuesta => respuesta.json())
-        .then(mostrarSeccion("paciente.html","Paciente"));
-      // await rellenarFormDatosPaciente(data);
+        .then(data => rellenarFormDatosPaciente(data)));
     })
 }
 
@@ -125,14 +129,6 @@ async function mostrarSeccion(archivo,titulo){
   tituloSeccion.innerHTML = await titulo;
 }
 
-const ObtenerJson = async(archivo,data)=>{
-  const respuesta = await fetch(archivo,{
-    method: "POST",
-    body: data
-  });
-  return respuesta.json();
-}  
-
  async function rellenarTablaPacientes(data){
   console.log(data)
   const tablaPacientes = document.getElementById("tablaPacientes");
@@ -140,7 +136,7 @@ const ObtenerJson = async(archivo,data)=>{
   const templateFila = document.getElementById("templateFila").content;
   const fragmento = document.createDocumentFragment();
   data.forEach(paciente => {
-    templateFila.querySelector(".fila").dataset.id = paciente.ID_DNI_;
+    templateFila.querySelector(".fila").dataset.id = paciente.ID_DNI;
     templateFila.querySelector(".dni").textContent = paciente.ID_DNI;
     templateFila.querySelector(".nombre").textContent = paciente.name;
     templateFila.querySelector(".apellido").textContent = paciente.surname;
@@ -152,7 +148,28 @@ const ObtenerJson = async(archivo,data)=>{
 }
 
 async function rellenarFormDatosPaciente(data){
-  console.log(data)  
+  const selectListObraSocial = document.getElementById("selectObraSocialPaciente")
+  console.log(selectListObraSocial.dataset)
+  obtenerOpcionesDatalist(selectListObraSocial.dataset.tabla,selectListObraSocial.dataset.columna_id,selectListObraSocial.dataset.columna_lista)
+  .then(opciones => {selectListObraSocial.innerHTML ='<option value="vacio" data-id="vacio"></option>'+opciones
+  console.log(data)
+  document.getElementById("inputDniPaciente").value = data[0].ID_DNI 
+  document.getElementById("inputCodigoPaciente").value = data[0].ID_PATIENT
+  document.getElementById("inputNombrePaciente").value = data[0].name
+  document.getElementById("inputApellidoPaciente").value = data[0].surname
+  document.getElementById("inputNacimientoPaciente").value = data[0].date_birth
+  document.getElementById("selectSexoPaciente").value = (data[0].gender == 'F' || data[0].gender =='M') ? data[0].gender : "vacio";
+  document.getElementById("inputTelefonoPaciente").value = data[0].phone
+  // document.getElementById("selectLocalidadPaciente").textContent = data.
+  // document.getElementById("inputCodigoPostal").textContent = data.
+  // document.getElementById("inputDireccionPaciente").textContent = data.
+  // document.getElementById("inputNDireccionPaciente").textContent = data.
+  selectListObraSocial.value =  selectListObraSocial.querySelector("option[data-id='"+data[0].PK_ID_PREPAID+"']").value
+  document.getElementById("inputNumeroAfiliadoPaciente").value = data[0].prepaid_number
+  document.getElementById("inputEmailPaciente").value = data[0].email
+
+
+  })
 }
 
 async function activarFuncionesSeccionFiltros(){
@@ -161,7 +178,7 @@ async function activarFuncionesSeccionFiltros(){
     obtenerOpcionesDatalist(navElement.dataset.table,navElement.dataset.columna_id,navElement.dataset.columna_lista)
     .then(opciones =>crearFilaDatalist(navElement,container,opciones))
   })
-
+ 
   document.getElementById("obtenerBusquedaFiltrado").addEventListener("click",()=>{
     data = new FormData();
     // data.append('ID_DNI', document.getElementById("inputBuscar").value);
@@ -174,17 +191,17 @@ async function activarFuncionesSeccionFiltros(){
       .then(respuesta => respuesta.json())
       .then( data => rellenarTablaPacientes(data))})
   })
+  // eliminarFiltroBadge();
 }
 
 function crearFilaDatalist(navElement,container,data){
-  console.log(data);
+  // console.log(data);
   const template = document.getElementById("containerDatalist").content;
   const cantidadFilas = container.querySelectorAll(".fila").length
   const fragment = document.createDocumentFragment();
+  const contenedorItemsFiltro = document.getElementById("contenedorFiltros")
   let idFila = 0
-  // si el total de filas es distinto de 0 entonces averiguo cual es la key de la última fila
   if(cantidadFilas!=0){
-    // a la última key le agregamos 1, esto lo hacemos así para no se creen keys repetidas
     idFila = parseInt(container.querySelectorAll(".fila")[cantidadFilas-1].dataset.id) + 1
   }
   console.log(idFila)
@@ -195,30 +212,66 @@ function crearFilaDatalist(navElement,container,data){
   template.querySelector("datalist").id = navElement.dataset.datalist + idFila
   template.querySelector("datalist").className = navElement.dataset.datalist + idFila
   fragment.appendChild(template.cloneNode(true))
+  
+  fragment.querySelector('.inputDatalist').addEventListener("change",(e)=>{
+    seleccionValida = false
+    container.querySelectorAll('.fila[data-id="'+ idFila +'"] option').forEach(opcion=>{
+      if(e.target.value == opcion.value){
+        if(contenedorItemsFiltro.querySelector('button[data-id="'+ navElement.dataset.datalist + idFila +'"]')){
+          contenedorItemsFiltro.querySelector("span").textContent = e.target.value;
+        }else{
+          let fragmento = document.createDocumentFragment();
+          let template = document.getElementById("opcionFiltro").content.cloneNode(true)
+          template.querySelector("span").textContent = e.target.value;
+          template.querySelector("button").dataset.id= navElement.dataset.datalist + idFila
+          fragmento.appendChild(template)
+          console.log(document.getElementById("contenedorFiltros"))
+          document.getElementById("contenedorFiltros").appendChild(fragmento)
+        }
+        seleccionValida = true
+      }
+      if( contenedorItemsFiltro.querySelector('button[data-id="'+ navElement.dataset.datalist + idFila +'"]') && seleccionValida == false){
+        contenedorItemsFiltro.removeChild(contenedorItemsFiltro.querySelector('button[data-id="'+ navElement.dataset.datalist + idFila +'"]'))
+        console.log("invalida")
+      }
+      console.log("cambio input")
+    })
+  })
 
   fragment.querySelector("button").addEventListener("click",(e)=>{
     if(e.currentTarget.querySelector("i").classList.contains("bx-minus")){
       console.log(e.currentTarget)
       container.removeChild(container.querySelector('.fila[data-id="'+ idFila +'"]'))
+      console.log(contenedorItemsFiltro);
+      contenedorItemsFiltro.removeChild(contenedorItemsFiltro.querySelector('button[data-id="'+ navElement.dataset.datalist + idFila +'"]'))
     }
-      
+    
     if(e.currentTarget.querySelector("i").classList.contains("bx-plus")){
       console.log("mas")
       e.currentTarget.querySelector("i").classList.replace("bx-plus","bx-minus")
       crearFilaDatalist(navElement,container,data)
     }
   })
-  container.appendChild(fragment)
-  console.log(navElement.dataset.bsTarget);
-  console.log(template);
-  console.log(container)
-  console.log(cantidadFilas)
-}
   
+  container.appendChild(fragment)
+}
+
+// function eliminarFiltroBadge(){
+
+//   document.querySelectorAll("i").forEach(icono=>{
+
+//     console.log(icono)
+//     icono.addEventListener("click",(e)=>{
+//       console.log(e.target)
+
+//   })
+//   })
+// }
+
 async function obtenerOpcionesDatalist(tabla,columnaId,columnaLista){
-  tabla = "PERSONAL_INFORMATION"
-  columnaId = "ID_DNI";
-  columnaLista = "name";
+  // tabla = "PERSONAL_INFORMATION"
+  // columnaId = "ID_DNI";
+  // columnaLista = "name";
   let data = new FormData();
   data.append("tabla",tabla);
   data.append("columnaId",columnaId);
@@ -230,7 +283,7 @@ async function obtenerOpcionesDatalist(tabla,columnaId,columnaLista){
   })
   const dataJson = await respuesta.json();
   await dataJson.forEach((opcion) => {
-    opciones += '<option data-id="' + opcion[columnaId] + '" value="' + opcion[columnaLista] +'">' 	
+    opciones += '<option data-id="' + opcion[columnaId] + '" value="' + opcion[columnaLista] +'">'+ opcion[columnaLista] +'</option>' 	
   })
   return await opciones;
 } 
--- php/obtenerAntecedentesInfoGeneralHistoriaClinica.php
+++ php/obtenerAntecedentesInfoGeneralHistoriaClinica.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 include("conexion.php");
 $dni = $_POST["ID_DNI"];
-$sql = "SELECT * FROM PERSONAL_INFORMATION WHERE ID_DNI=".$dni;
+$sql = "SELECT * FROM `PATIENT` INNER JOIN `PERSONAL_INFORMATION` ON PATIENT.PK_ID_DNI = PERSONAL_INFORMATION.ID_DNI WHERE PERSONAL_INFORMATION.ID_DNI = $dni";
 $consultaSql = $conexion->query($sql);
 if(!empty($consultaSql) AND mysqli_num_rows($consultaSql)>0){
     $data = $consultaSql->fetch_all(MYSQLI_ASSOC);
