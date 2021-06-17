19f65d0d9f98422bb17bcdb7cec083bca705e5e4
Alexis <alexislazarte2003@gmail.com>
2021-06-17 09:57:29 -0300

    Probando el registro

--- php/Registro.php
+++ php/Registro.php
@@ -1,25 +1,25 @@
 <?php
-include("conexion.php");
-$dni = $_POST['dni']; 
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$fechaNacimieto = $_POST['fechaNacimiento'];
-$direccion = $_POST['direccion'];
-$direccionN = $_POST['direccionN'];
-$telefono = $_POST['telefono'];
-$email = $_POST['email'];
-$contrasena = $_POST['contrasena'];
-$sql = "INSERT INTO PERSONAL_INFORMATION (ID_DNI,name,surname,date_birth,phone,email) VALUES ($dni,$nombre,$apellido,$fechaNacimieto,$telefono,$email)";
-$consultaSql = $conexion->query($sql);
-// $resultado = $consultaSql->fetch_all(MYSQLI_ASSOC);
 
-// $sql = "INSERT INTO USER VALUES ($dni,$nombre,$apellido,$fechaNacimieto,$email) ";
-// $consultaSql = $conexion->query($sql);
-// $resultado = $consultaSql->fetch_all(MYSQLI_ASSOC);
+    include("conexion.php");
+    $dni = $_POST['dni']; 
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $fechaNacimieto = $_POST['fechaNacimiento'];
+    $direccion = $_POST['direccion'];
+    $direccionN = $_POST['direccionN'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+    $contrasena = $_POST['contrasena'];
+    $sql = "INSERT INTO PERSONAL_INFORMATION (ID_DNI,name,surname,date_birth,phone,email) VALUES ($dni,$nombre,$apellido,$fechaNacimieto,$telefono,$email)";
+    if (mysqli_query($conexion, $sql)) {
+        echo "Funciono";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+    }
+    $consultaSql = $conexion->query($sql);
 
+    $conexion->close();
 
-$conexion->close();
-// echo $resultado;
 ?>
 
 
