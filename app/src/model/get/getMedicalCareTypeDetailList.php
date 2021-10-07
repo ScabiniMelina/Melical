bdde6fec7449660c0143434e04017a448a756be0
Scabini Melina Noelia <melina09321@gmail.com>
2021-10-07 15:50:33 -0300

    fix: selects de la sección de filtros

--- app/src/model/get/getMedicalCareTypeDetailList.php
+++ app/src/model/get/getMedicalCareTypeDetailList.php
@@ -1,10 +1,7 @@
 <?php
 include("./../connection.php");
 $medicalCareType = $_GET['condition'];
-$sql = "SELECT TYPE_DETAIL.ID_TYPE_DETAIL, TYPE_DETAIL.name FROM TYPE_DETAIL INNER JOIN CLASSIFICATION_DETAIL ON CLASSIFICATION_DETAIL.ID_CLASSIFICATION_DETAIL = TYPE_DETAIL.PK_ID_CLASSIFICATION_DETAIL WHERE CLASSIFICATION_DETAIL.name=? ";
-$parameters =  array($medicalCareType);
-$typeOfParameters = "s";
-$result = getPreparedStatement($sql,$typeOfParameters, $parameters);
-$data = getResultOfPreparedStatement($result);
+$sql = "SELECT TYPE_DETAIL.ID_TYPE_DETAIL, TYPE_DETAIL.name FROM TYPE_DETAIL INNER JOIN CLASSIFICATION_DETAIL WHERE CLASSIFICATION_DETAIL.ID_CLASSIFICATION_DETAIL = TYPE_DETAIL.PK_ID_CLASSIFICATION_DETAIL && CLASSIFICATION_DETAIL.name='$medicalCareType'";
+$data = query($sql);
 echo json_encode($data);
 ?>
