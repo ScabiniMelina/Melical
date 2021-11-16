0c1024886428447f314e68ee021280e4afae2558
Scabini Melina Noelia <melina09321@gmail.com>
2021-11-16 19:48:20 -0300

    fix: se muestran todas las tablas con sus respectivos paginadores

--- app/src/model/get/getRooms.php
+++ app/src/model/get/getRooms.php
@@ -1,13 +1,17 @@
 <?php
-
+session_start();
 include("./../connection.php");
-$numberOfResultsPerPage = 10;
+$numberOfResultsPerPage = 2;
 $currentPage = getCurrentPage();
 $initialLimit = getPaginationConfig($numberOfResultsPerPage,$currentPage);
-$amountOfPages = getAmountOfPagesToCreatePager("Sector_Hospital",$numberOfResultsPerPage);
-$sql = "SELECT Sector.name AS seccion, Tier_Sector.tier AS piso,Count.status AS estado, Count.number AS numero FROM `Tier_Sector` INNER JOIN Sector_Hospital on Sector_Hospital.ID_SH=Tier_Sector.PK_SH INNER JOIN Sector ON Sector.ID_SECTOR=Sector_Hospital.PK_Sector INNER JOIN Room ON Tier_Sector.ID_tier=Room.PK_TS INNER JOIN Count ON Count.PK_Room=Room.ID_room where Sector_Hospital.PK_Hospital='1'";
-$result =  executePreparedStatement($sql,null, null);
+$parameters = array($_SESSION['ID_HOSPITAL']);
+$typeOfParameters = "i";
+$amountOfPages = getAmountOfPagesToCreatePager(" `Tier_Sector` INNER JOIN Sector_Hospital on Sector_Hospital.ID_SH=Tier_Sector.PK_SH INNER JOIN Sector ON Sector.ID_SECTOR=Sector_Hospital.PK_Sector INNER JOIN Room ON Tier_Sector.ID_tier=Room.PK_TS INNER JOIN Count ON Count.PK_Room=Room.ID_room where Sector_Hospital.PK_Hospital= ? ", $numberOfResultsPerPage,$typeOfParameters,$parameters);
+$sql = "SELECT Sector.name AS seccion, Tier_Sector.tier AS piso,Count.status AS estado, Count.number AS numero FROM `Tier_Sector` INNER JOIN Sector_Hospital on Sector_Hospital.ID_SH=Tier_Sector.PK_SH INNER JOIN Sector ON Sector.ID_SECTOR=Sector_Hospital.PK_Sector INNER JOIN Room ON Tier_Sector.ID_tier=Room.PK_TS INNER JOIN Count ON Count.PK_Room=Room.ID_room where Sector_Hospital.PK_Hospital='1' LIMIT ?,?";
+$typeOfParameters = "ii";
+$parameters = array($initialLimit, $numberOfResultsPerPage);
+$result =  executePreparedStatement($sql,$typeOfParameters, $parameters);
 $data =  getResultOfPreparedStatement($result); 
-$msg = ['type'=>'success','text'=>'todoCorrecto'];
-sendJson($data,$msg,null,null,null);
+sendJson($data,$msg,null, $amountOfPages, $currentPage);
 ?>
+
--- app/src/model/get/getStudies.php
+++ app/src/model/get/getStudies.php
@@ -1,13 +1,16 @@
 <?php
-
+session_start();
 include("./../connection.php");
-$numberOfResultsPerPage = 10;
+$numberOfResultsPerPage = 2;
 $currentPage = getCurrentPage();
-$initialLimit = getPaginationConfig($numberOfResultsPerPage,$currentPage);
-$amountOfPages = getAmountOfPagesToCreatePager("Sector_Hospital",$numberOfResultsPerPage);
-$sql = "SELECT Personal_Information.DNI AS dni, Personal_Information.name AS nombre, Personal_Information.surname AS apellido, Study_Detail.date_made AS fecha, Type.name AS tipo from Personal_Information INNER JOIN Patient ON Patient.PK_Dni=Personal_Information.ID_DNI INNER JOIN Medical_Care ON Medical_Care.PK_Patient=Patient.ID_PATIENT INNER JOIN Sector_Patient ON Sector_Patient.PK_MC=Medical_Care.ID_MC INNER JOIN Sector_Hospital ON Sector_Patient.PK_SH=Sector_Hospital.ID_SH INNER JOIN Study_Detail ON Study_Detail.PK_MC=Medical_Care.ID_MC INNER JOIN Type ON Type.ID_TYPE=Study_Detail.PK_TYPE WHERE Sector_Hospital.PK_Hospital='1'";
-$result =  executePreparedStatement($sql,null, null);
-$data =  getResultOfPreparedStatement($result); 
-$msg = ['type'=>'success','text'=>'todoCorrecto'];
-sendJson($data,$msg,null,null,null);
-?>
+$initialLimit = getPaginationConfig($numberOfResultsPerPage, $currentPage);
+$parameters = array($_SESSION['ID_HOSPITAL']);
+$typeOfParameters = "i";
+$sql2 = "Personal_Information INNER JOIN Patient ON Patient.PK_Dni=Personal_Information.ID_DNI INNER JOIN Medical_Care ON Medical_Care.PK_Patient=Patient.ID_PATIENT INNER JOIN Sector_Patient ON Sector_Patient.PK_MC=Medical_Care.ID_MC INNER JOIN Sector_Hospital ON Sector_Patient.PK_SH=Sector_Hospital.ID_SH INNER JOIN Study_Detail ON Study_Detail.PK_MC=Medical_Care.ID_MC INNER JOIN Type ON Type.ID_TYPE=Study_Detail.PK_TYPE WHERE Sector_Hospital.PK_Hospital=?";
+$amountOfPages = getAmountOfPagesToCreatePager($sql2, $numberOfResultsPerPage, $typeOfParameters, $parameters);
+$sql = "SELECT Personal_Information.DNI AS dni, Personal_Information.name AS nombre, Personal_Information.surname AS apellido, Study_Detail.date_made AS fecha, Type.name AS tipo from Personal_Information INNER JOIN Patient ON Patient.PK_Dni=Personal_Information.ID_DNI INNER JOIN Medical_Care ON Medical_Care.PK_Patient=Patient.ID_PATIENT INNER JOIN Sector_Patient ON Sector_Patient.PK_MC=Medical_Care.ID_MC INNER JOIN Sector_Hospital ON Sector_Patient.PK_SH=Sector_Hospital.ID_SH INNER JOIN Study_Detail ON Study_Detail.PK_MC=Medical_Care.ID_MC INNER JOIN Type ON Type.ID_TYPE=Study_Detail.PK_TYPE WHERE Sector_Hospital.PK_Hospital= ? LIMIT ?,?";
+$typeOfParameters = "iii";
+$parameters = array($_SESSION['ID_HOSPITAL'], $initialLimit, $numberOfResultsPerPage);
+$result =  executePreparedStatement($sql, $typeOfParameters, $parameters);
+$data =  getResultOfPreparedStatement($result);
+sendJson($data, $msg, null, $amountOfPages, $currentPage);
