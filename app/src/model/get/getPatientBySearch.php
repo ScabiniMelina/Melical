d8d55717a8cf83114b67fb5dd79119daab29458b
Scabini Melina Noelia <melina09321@gmail.com>
2021-11-25 01:18:14 -0300

    fix: filtrado de dni de busqueda de pacientes

--- app/src/model/get/getPatientBySearch.php
+++ app/src/model/get/getPatientBySearch.php
@@ -3,13 +3,15 @@ include("./../connection.php");
 $numberOfResultsPerPage = 9;
 $currentPage = getCurrentPage();
 $initialLimit = getPaginationConfig($numberOfResultsPerPage, $currentPage);
-$sql = "SELECT ID_DNI AS ID, dni, name, surname, date_birth AS dateBirth FROM Personal_Information ";
+$id = $_GET["id"];
+$sql = "SELECT ID_DNI AS ID,ID_DNI AS oldId, dni, name, surname, date_birth AS dateBirth FROM Personal_Information ";
 $condition = " WHERE ";
-$id = $_GET['id'];
 if (isset($id)) {
+    $parameters =  array($id);
     if (is_numeric($id)) {
-        $condition .= "  dni = ?";
-        $typeOfParameters = "i";
+        $condition .= " dni LIKE ?";
+        $typeOfParameters = "s";
+        //$var="%$id%";
         $parameters = array("%$id%");
     } else {
         $condition .= " name LIKE ? OR surname LIKE ?";
@@ -17,9 +19,14 @@ if (isset($id)) {
         $parameters = array("%$id%", "%$id%");
     }
 }
+
+$result = executePreparedStatement($sql, $typeOfParameters, $parameters);
+$data = getResultOfPreparedStatement($result);
 $amountOfPages = getAmountOfPagesToCreatePager("Personal_Information" . $condition, $numberOfResultsPerPage, $typeOfParameters, $parameters);
 $typeOfParameters .= "ii";
 array_push($parameters, $initialLimit, $numberOfResultsPerPage);
 $result = executePreparedStatement($sql . $condition . " LIMIT ?,? ", $typeOfParameters, $parameters);
 $data = getResultOfPreparedStatement($result);
 sendJson($data, $msg, null, $amountOfPages, $currentPage);
+
+// sendJson($data, $msg, null, null, null);
